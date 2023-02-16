47950b4fa4086bbae33fb14340ba4b15c9b20d69
arceusWitness <saant2016@gmail.com>
2023-02-16 15:12:48 -0300

    k

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
+
 const Budget = () => {
-  const { budget } = useContext(AppContext);
+  const { budget, dispatch } = useContext(AppContext);
+  const [inputBudget, setInputBudget] = useState(budget);
+
+  const handleBudgetChange = (event) => {
+    const newBudget = parseInt(event.target.value);
+    setInputBudget(newBudget);
+    dispatch({ type: "SET_BUDGET", payload: newBudget });
+  };
+
   return (
     <div className="alert alert-secondary">
-      <span>Budget: £{budget}</span>
+      <label>
+        Budget: £
+        <input
+          type="number"
+          id="Name"
+          name="Name"
+          value={inputBudget}
+          max="20000"
+          onChange={handleBudgetChange}
+          step={10}
+        />
+      </label>
     </div>
   );
 };
+
 export default Budget;
